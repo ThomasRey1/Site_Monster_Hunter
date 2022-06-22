4f48252d33ce08c234206ff5fa1cfa516611e8b4
ThomasRey1 <thorey@etmlnet.local>
2022-06-22 07:59:37 +0200

    détail

--- Site_Monster_Hunter/resources/views/maps.blade.php
+++ Site_Monster_Hunter/resources/views/maps.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <div class="bg-[#E5D4BA] dark:bg-[#1C1C1C] w-[75%] mt-[50px] duration-[0.5s] h-screen text-black dark:text-[#FFF6E5]">
-        <p class="text-center text-6xl mb-[100px]">Map</p>
-        <div class="grid grid-cols-3 place-items-center	mb-[35px]">
+    <div class="bg-[#E5D4BA] dark:bg-[#1C1C1C] w-[75%] mt-[50px] duration-[0.5s] text-black dark:text-[#FFF6E5]">
+        <p class="text-center text-6xl mb-[203px]">Map</p>
+        <div class="grid grid-cols-3 place-items-center	mb-[50px]">
             @foreach ($maps as $map)
             <div class="text-center mb-[100px] text-xl">
                 <a href="{{ route('map.show', ['id' => $map->id]) }}">
